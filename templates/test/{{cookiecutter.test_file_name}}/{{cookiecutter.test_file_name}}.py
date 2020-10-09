f7724f5cf48fd9a8a5f2bc1d29d775a1a74db32e
Chris Stead <cmstead@gmail.com>
2020-10-09 16:20:57 -0700

    Added PyInquirer snippets

---
+++ templates/test/{{cookiecutter.test_file_name}}/{{cookiecutter.test_file_name}}.py
@@ -0,0 +1,5 @@
+from {{cookiecutter.package_name}} import {{cookiecutter.module_name}}
+
+def test_correctness_test_works():
+    """This is a correctness test -- delete it when it passes"""
+    assert true == true
