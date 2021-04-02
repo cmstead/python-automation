96bef8ad4eec880f71127976c38d783809acf468
Chris Stead <cmstead@gmail.com>
2021-04-02 14:11:56 -0700

    Updated current templates, removed cruft

--- templates/flask-project/{{cookiecutter.project_name}}/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-set FLASK_APP=advanced-analytics-api/__main__.py
-python -m flask run
---
+++ templates/library_project/{{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,22 @@
+import pathlib
+from setuptools import setup, find_packages
+
+CURRENT_DIR = pathlib.Path(__file__).parent
+README_CONTENT = (CURRENT_DIR / "README.md").read_text()
+
+setup(
+    name='{{cookiecutter.project_name}}',
+    version='0.1.0',
+    packages=["{{cookiecutter.source_directory}}"],
+    author="{{cookiecutter.author}}",
+    author_email="{{cookiecutter.email}}",
+    url="https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.project_name}}#readme",
+    platforms=['any'],
+    description="{{cookiecutter.description}}",
+    license='{{cookiecutter.license}}',
+    long_description=README_CONTENT,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: {{cookiecutter.python_version}}"
+    ]
+)
